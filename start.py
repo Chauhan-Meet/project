85275b23124af48da9505c949dc4fbac3caf8e01
Chauhan-Meet <99122973+Chauhan-Meet@users.noreply.github.com>
2022-03-08 18:15:16 +0530

    Add files via upload
    changes made on 8 th MARCH

--- start.py
+++ start.py
@@ -1,32 +1,84 @@
+try:
+    import mysql.connector as sq
+    p=sq.connect(host="localhost",user="root",password="",database="library")
+    if p.is_connected()==False:
+        print("Error..")
+    else:
+        myc=p.cursor()
+except:
+    print("ERROR")
 def Add_book():
-    print("YES")
+    Bid=int(input("Enter book id:"))
+    book=input("Enter book name:")
+    au=input("Enter Author name:")
+    pr=float(input("Enter price:"))
+    copies=int(input("Enter number of copies:"))
+    qr="INSERT INTO book VALUES({},'{}','{}',{},{},{})".format(Bid,book,au,pr,copies,copies)
+    myc.execute(qr)
+    p.commit()
+    print("Book was added successfully")
+def Edit_book():
+    Id=int(input("Enter book id:"))
+    qry="SELECT* FROM book WHERE Bookid={}".format(Id)
+    myc.execute(qry)
+    R=myc.fetchone()
+    if R:
+        npr=float(input("Enter new price of book:"))
+        q="UPDATE book SET Price={} WHERE Bookid={}".format(npr,Id)
+        myc.execute(q)
+        p.commit()
+        print("The price was edited successfully")
+    else:
+        print("You have entered wrong book id")
+def Delete_book():
+    Id=int(input("Enter book id:"))
+    qry="SELECT* FROM book WHERE Bookid={}".format(Id)
+    myc.execute(qry)
+    R=myc.fetchone()
+    if R:
+        q="DELETE FROM book WHERE Bookid={}".format(Id)
+        myc.execute(q)
+        p.commit()
+        print("The book was deleted successfully")
+    else:
+        print("You have entered wrong book id")
+def Search_book():
+    Id=int(input("Enter book id:"))
+    qry="SELECT* FROM book WHERE Bookid={}".format(Id)
+    myc.execute(qry)
+    R=myc.fetchone()
+    if R:
+        print(R)
+    else:
+        print("You have entered wrong book id")
+
 while True:
     print("="*80)
     print("\t\t\t-------LIBRARY MANAGEMENT-------")
     print("="*80)
     print("\t\t\t\tChoose an action:\n\t\t\t\t1.Book details.\n\t\t\t\t2.Member details.\n\
            \t\t\t3.Issue or return a book.\n\t\t\t\t4.Information.\n\t\t\t\t5.EXIT")
-    a=int(input("What is youe action ? \n=="))
+    a=int(input())
     if a==1:
         while True:
             print("\t\t\t\tChoose an action:\n\t\t\t\t1.Add book.\n\t\t\t\t2.Edit book details.\n\
         \t\t\t3.Delete a book.\n\t\t\t\t4.Search for a book.\n\t\t\t\t5.Back to main menu")
-            ch=int(input("What is youe action ? \n=="))
+            ch=int(input())
             if ch==1:
                 Add_book()
             elif ch==2:
-                print("2")
+                Edit_book()
             elif ch==3:
-                print("3")
+                Delete_book()
             elif ch==4:
-                print("4")
+                Search_book()
             elif ch==5:
                 break
     elif a==2:
         while True:
             print("\t\t\t\tChoose an action:\n\t\t\t\t1.Add member.\n\t\t\t\t2.Edit member details\
     .\n\t\t\t\t3.Delete a member.\n\t\t\t\t4.Search for a member.\n\t\t\t\t5.Back to main menu")
-            c=int(input("What is youe action ? \n=="))
+            c=int(input())
             if c==1:
                 print("1")
             elif c==2:
@@ -41,7 +93,7 @@ while True:
         while True:
             print("\t\t\t\tChoose an action:\n\t\t\t\t1.Issue book.\n\t\t\t\t2.Return book.\n\
             \t\t\t3.Back to main menu")
-            h=int(input("What is youe action ? \n=="))
+            h=int(input())
             if h==1:
                 print("1")
             elif h==2:
@@ -52,7 +104,7 @@ while True:
         while True:
             print("\t\t\t\tChoose an action:\n\t\t\t\t1.Related to Book .\n\t\t\t\t2.Related \
    Member\n\t\t\t\t3.Issue details.\n\t\t\t\t4.Return details.\n\t\t\t\t5.Back to main menu.")
-            x=int(input("What is youe action ? \n=="))
+            x=int(input())
             if x==1:
                 print("1")
             elif x==2:
@@ -65,4 +117,5 @@ while True:
                 break
     elif a==5:
         break
+    
   
